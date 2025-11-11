d97bbfc7c9212e91938c75b55e07d58595be0127
Giuleppe09 <giuseppe.castaldo22@studenti.unina.it>
2025-11-12 00:11:46 +0100

    LLSint.csv

--- WorkLoad_Characterization/script.ps1
+++ WorkLoad_Characterization/script.ps1
@@ -14,8 +14,8 @@ $JMX_DIR = $WorkDir # Usa la cartella dello script
 $JTL_DIR = $WorkDir # Usa la cartella dello script
 
 # --- Configurazione Esecuzione ---
-$jmxFileToRun = "TestPlan_WorkLoadCharacterization.jmx"
-$jtlOutputFileName = "HL1_results.csv" # Nome del file JTL di output
+$jmxFileToRun = "TestPlan_Filtered.jmx"
+$jtlOutputFileName = "HL_Sint_results.csv" # Nome del file JTL di output
 
 $testDurationMinutes = 25
 
